079df987aea2e934ed5547b43b240e21244393d6
Michael Rolle <michael.rolle@bbd.co.za>
2024-03-30 22:42:13 +0200

    Added frontend terraform for infrastructure

---
+++ terraform/frontend/variables.tf
@@ -0,0 +1,34 @@
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags applied to all resources"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the resources will be deployed."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC."
+}
+
+variable "vpc_az" {
+  type        = string
+  description = "The availability zone for the VPC."
+}
+
+variable "vpc_public_subnet" {
+  type        = string
+  description = "The public subnet for the VPC."
+}
+
+variable "ec2_public_key" {
+  type        = string
+  description = "The public key to use for the EC2 instance."
+}
+
+variable "naming_prefix" {
+  type        = string
+  description = "The prefix to use for naming resources."
+}

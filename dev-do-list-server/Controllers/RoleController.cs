80eba48e5a7b5e86dd197815219f670059db4f5a
motha003 <avishkar.motheelal11@gmail.com>
2024-04-06 15:03:37 +0200

    Add role DTO and update controller

--- dev-do-list-server/Controllers/RoleController.cs
+++ dev-do-list-server/Controllers/RoleController.cs
@@ -1,82 +1,67 @@
 ﻿using DevDoListServer.Models;
 using DevDoListServer.Repositories;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace DevDoListServer.Controllers
+namespace DevDoListServer.Controllers;
+
+[Route("api/v1/role")]
+[ApiController]
+public class RoleController(RoleRepository roleRepository) : ControllerBase
 {
-    [Route("api/v1/role")]
-    [ApiController]
-    public class RoleController(RoleRepository roleRepository) : ControllerBase
+    private readonly RoleRepository roleRepository = roleRepository;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RoleDto>>> GetRoles()
     {
-        private readonly RoleRepository roleRepository = roleRepository;
+        var roles = await roleRepository.GetAll();
+        var roleDtos = roles.Select(role => new RoleDto(role));
+        return Ok(roleDtos);
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Role>>> GetRoles()
+    [HttpGet("{id}")]
+    public async Task<ActionResult<RoleDto>> GetRole([FromRoute] int id)
+    {
+        var role = await roleRepository.GetById(id);
+
+        if (role == null) return NotFound("Role not found");
+
+        return new RoleDto(role);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutRole([FromRoute] int id, [FromBody] Role role)
+    {
+        if (id != role.RoleId) return BadRequest();
+
+        try
         {
-            return await roleRepository.GetAll();
+            return Ok(await roleRepository.Update(role));
         }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Role>> GetRole([FromRoute] int id)
+        catch (DbUpdateConcurrencyException)
         {
-            var role = await roleRepository.GetById(id);
-
-            if (role == null)
-            {
+            if (!await roleRepository.Exists(e => e.RoleId == id))
                 return NotFound("Role not found");
-            }
-
-            return role;
+            throw;
         }
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutRole([FromRoute] int id, [FromBody] Role role)
-        {
-            if (id != role.RoleId)
-            {
-                return BadRequest();
-            }
+    [HttpPost]
+    public async Task<ActionResult<RoleDto>> PostRole([FromBody] Role role)
+    {
+        var createdRole = await roleRepository.Create(role);
+        var roleDto = new RoleDto(createdRole);
+        return CreatedAtAction("GetRole", new { id = roleDto.RoleId }, roleDto);
+    }
 
-            try
-            {
-               return Ok(await roleRepository.Update(role));
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!await roleRepository.Exists(e => e.RoleId == id))
-                {
-                    return NotFound("Role not found");
-                }
-                else
-                {
-                    throw;
-                }
-            }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteStatus([FromRoute] int id)
+    {
+        var role = await roleRepository.GetById(id);
+        if (role == null) return NotFound("Role not found");
 
-        }
+        await roleRepository.Delete(role);
 
-        [HttpPost]
-        public async Task<ActionResult<Status>> PostRole([FromBody] Role role)
-        {
-            var createdRole = await roleRepository.Create(role);
-
-            return CreatedAtAction("GetRole", new { id = createdRole.RoleId }, createdRole);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteStatus([FromRoute] int id)
-        {
-            var role = await roleRepository.GetById(id);
-            if (role == null)
-            {
-                return NotFound("Role not found");
-            }
-
-            await roleRepository.Delete(role);
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
---
+++ dev-do-list-server/Models/RoleDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DevDoListServer.Models;
+
+public class RoleDto
+{
+    public RoleDto(Role role)
+    {
+        RoleId = role.RoleId;
+        RoleType = role.RoleType;
+    }
+
+    public int RoleId { get; set; }
+
+    public string RoleType { get; set; }
+}

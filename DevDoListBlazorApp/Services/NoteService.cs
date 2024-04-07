aaa1fa69ddf6dda7cead9abdbc79cc6f1fd9bd20
Sasha Kingwill <sasha-lee.kingwill@bbd.co.za>
2024-04-07 21:21:31 +0200

    Added services

--- DevDoListBlazorApp/Services/NoteService.cs
+++ DevDoListBlazorApp/Services/NoteService.cs
@@ -1,22 +1,23 @@
+﻿using System.Collections.Generic;
 using System.Text.Json;
 using DevDoListBlazorApp.Models;
 using DevDoListBlazorApp.Utils;
 
 namespace DevDoListBlazorApp.Services;
 
-public class UserService(string accessToken)
+public class NoteService(string accessToken)
 {
     private readonly string _serverUrl = FuncUtils.GetServerUrl();
 
-    public async Task<User?> GetUserByUsername()
+    public async Task<List<Note>?> GetAllNotes()
     {
         var client = new HttpClient();
-        var request = new HttpRequestMessage(HttpMethod.Get, _serverUrl + "api/v1/user/loggedIn");
+        var request = new HttpRequestMessage(HttpMethod.Get, _serverUrl + "api/v1//task");
         request.Headers.Add("Authorization", "Bearer " + accessToken);
         var response = await client.SendAsync(request);
         if (!response.IsSuccessStatusCode) return null;
-        var user = JsonSerializer.Deserialize<User>(await response.Content.ReadAsStringAsync())!;
-        return user;
+        var task = JsonSerializer.Deserialize <List<Note>> (await response.Content.ReadAsStringAsync())!;
+        return task;
 
     }
 }

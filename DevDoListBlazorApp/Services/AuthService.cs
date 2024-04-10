4ffcf4dd873e47d61f18ff0acd9a8a73d4fa428f
motha003 <avishkar.motheelal11@gmail.com>
2024-04-10 18:49:01 +0200

    Add logged in method to auth service

--- DevDoListBlazorApp/Services/AuthService.cs
+++ DevDoListBlazorApp/Services/AuthService.cs
@@ -3,4 +3,9 @@
 public static class AuthService
 {
     public static string? accessToken { get; set; }
+
+    public static bool IsLoggedIn()
+    {
+        return accessToken is not null;
+    }
 }
